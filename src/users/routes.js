a5c1e9b94da335f0d7c78027dba1b95c031280ca
RaydomeNT <raydome85@gmail.com>
2022-08-16 17:41:33 +0100

    testing CRUD functions, token auth not not working

--- src/users/routes.js
+++ src/users/routes.js
@@ -1,14 +1,14 @@
 const { Router } = require("express");
 const userRouter = Router();
-const { createUser, login, getAllUsers, updateEmail, deleteUser } = require("./controllers");
-const { hashPass, comparePasswords, tokenAuth } = require("../middleware");
+const { createUser, login, getAllUsers, updateEmail, deleteUser, updateUser } = require("./controllers");
+const { hashPass, comparePasswords, tokenAuth, newHashPass } = require("../middleware");
 
 userRouter.post("/user", hashPass, createUser);
 userRouter.post("/login", comparePasswords, login);
 userRouter.get("/list", getAllUsers);
-userRouter.get("/login", tokenAuth, login);
+userRouter.get("/login", tokenAuth, login); //not currently working
 userRouter.patch("/update", updateEmail);
 userRouter.delete("/delete", deleteUser);
-// userRouter.patch('/user', comparePasswords, newHashPass, updateUser);
+userRouter.patch('/user', comparePasswords, newHashPass, updateUser);
 
 module.exports = userRouter;
