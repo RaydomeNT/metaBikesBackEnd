a0bf69b3d83adabbbebae7f128a5e5aec9bef682
RaydomeNT <raydome85@gmail.com>
2022-08-22 17:00:16 +0100

    port 5001 process dot env

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const userRouter = require("./users/routes");
 const app = express();
 const cors = require("cors")
+const serverPort = 5001;
+const port = process.env.PORT || serverPort;
 
 //add relevant routes and controllers to app before listen runs, like CRUD
 app.use(express.json()); //tell entire server that it will aslways recieve json and always send back json, parsed automatically
 app.use(cors());
 app.use(userRouter);
 
-app.listen(5001, () => {
-  console.log("listening on port 5001");
+app.listen(port, () => {
+  console.log("listening on port", port);
 });
