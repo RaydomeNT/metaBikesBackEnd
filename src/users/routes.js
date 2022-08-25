3c36413a1e7bbc2809ede9eea1c947de745cb0d9
RaydomeNT <raydome85@gmail.com>
2022-08-25 16:57:45 +0100

    email check

--- src/users/routes.js
+++ src/users/routes.js
@@ -9,6 +9,6 @@ userRouter.get("/list", getAllUsers);
 userRouter.get("/login", tokenAuth, login);
 userRouter.patch("/update", updateEmail);
 userRouter.delete("/delete", deleteUser);
-userRouter.patch('/user', comparePasswords, newHashPass, updateUser);
+userRouter.patch('/user', emailCheck, comparePasswords, newHashPass, updateUser);
 
 module.exports = userRouter;
